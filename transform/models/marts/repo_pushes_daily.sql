950bc39de1ca09318b50ee2170693d683ed9e264
KeNin <you@example.com>
2024-10-29 13:54:56 -0700

    Optimize duplicate code

--- transform/models/marts/repo_pushes_daily.sql
+++ transform/models/marts/repo_pushes_daily.sql
@@ -3,7 +3,7 @@ SELECT
     repo_name,
     DATE_TRUNC('day', event_date) AS date_day,
     COUNT(*) AS push_count,
-    SUM(COUNT(*)) OVER (PARTITION BY repo_id ORDER BY DATE_TRUNC('day', event_date) ASC) AS cumul_push_count
+    SUM(push_count) OVER (PARTITION BY repo_id ORDER BY date_day ASC) AS cumul_push_count
 FROM {{ ref("stg_gharchive")}}
 WHERE event_type = 'Push'
 GROUP BY 1, 2, 3
